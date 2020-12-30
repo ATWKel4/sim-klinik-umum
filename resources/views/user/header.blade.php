0a9b087040c71b2e24935c417bdf56f975f83dcf
nabilazalfa <54559619+nabilazalfa@users.noreply.github.com>
2020-12-30 08:55:12 +0700

    Update header.blade.php

--- resources/views/user/header.blade.php
+++ resources/views/user/header.blade.php
@@ -29,7 +29,6 @@
         <li class="active"><a href="index.html">Beranda</a></li>
         <li><a href="#about">Tentang klinik</a></li>
         <li><a href="#services">Pelayanan</a></li>
-        <!-- <li><a href="#departments">Departments</a></li> -->
         <li><a href="#Dokter">Dokter</a></li>
         </li>
         <li><a href="#contact">Kontak kami</a></li>
@@ -37,7 +36,7 @@
     </ul>
     </nav><!-- .nav-menu -->
 
-    <a href="#appointment" class="appointment-btn scrollto"><b>Jadwalkan konsultasi</b></a>
+    <a href="#appointment" class="appointment-btn scrollto"><b>Login</b></a>
 
 </div>
 </header><!-- End Header -->
